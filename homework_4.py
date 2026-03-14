ce2e2f2dff75f6e3c4669034952a5c41cba3f22c
Pavlo <pavlikbartmanskiy@gmail.com>
2026-03-15 00:08:45 +0200

    bot_assistant

---
+++ homework_4.py
@@ -0,0 +1,68 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            if len(args[0]) == 0:
+                return "Give me name and phone please."
+            elif len(args[0]) == 1:
+                return "Give me a phone please."
+        except IndexError:
+            return "Give me a name please"
+    return inner
+
+@input_error
+def add_contact(args, contacts):
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."      
+ 
+def change_username_phone(args, contacts):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        return "Contact not found."
+    
+@input_error
+def phone_username(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return contacts[name]
+    else:
+        return "Contact name not found"
+    
+def all_users(args, contacts):
+    return contacts
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_username_phone(args, contacts))
+        elif command == "phone":
+            print(phone_username(args, contacts))
+        elif command == "all":
+            print(all_users(args, contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
